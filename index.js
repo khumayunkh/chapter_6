e63f15a25f3e3e4d56c1da95e5c586d681535129
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-27 22:17:55 +0500

    create object

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log('hello')
+
+let x = new Object({  // create new obj with 'new Object'
+    name:'Humoyun', 
+    age:19, 
+    book : new Object({ // another Obj in Object x
+        name:'Harry Poter',
+        price: '200$'
+    })
+})
+// similar
+let y = {
+    name:'Humoyun', 
+    age:19, 
+    book : new Object({ // another Obj in Object x
+        name:'Harry Poter',
+        price: '200$'
+    })
+}
+console.log(y.book.price)
+
+
+let person = [
+    {name:'Humoyun', age:18},
+    {name:'Kamran', age:22}
+]
+
+let z = person.map(person => person.name)
+console.log(z) // Humoyun and Kamran
+
