1d1c6ed6cd1377edb84b3d1690e33982fae335bd
khumayunkh <hasanovhumayun15@gmail.com>
2022-05-27 23:15:45 +0500

    inheritance

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // console.log('hello')
-
+// =======================
 // let x = new Object({  // create new obj with 'new Object'
 //     name:'Humoyun', 
 //     age:19, 
@@ -18,12 +18,18 @@ let y = {
     })
 }
 console.log(y.book.price)
-
+// =======================
 
 let person = [
     {name:'Humoyun', age:18},
     {name:'Kamran', age:22}
 ]
+// ===============
+console.log(person[0]['age'])
+// similar ====== 
+console.log(person[0].age) 
+// ================
+
 
 let z = person.map(person => person.name)
 console.log(z) // Humoyun and Kamran
@@ -42,4 +48,17 @@ let x = Object.create({},{
 x.name = "Humo" //false because of 'configurable is false'
 
 console.log('x ',x.name) //show in console, but can not change the value because of 'writable is false'
-console.log('x ',x.age) // does not show in console because of 'enumerable is false'
+for(let d in x){
+    console.log(d)
+} // age does not show in console because of 'enumerable is false'
+
+
+let a = {}
+a.x = 3
+let b = Object.create(a)  // inherits the key and value of a
+console.log(b) // {x:3}
+b.y = 4
+let c = Object.create(b) // inherits the keys and values of a and b
+console.log(c)  // {y:4} and {x:4}
+let d = c.x * c.y
+console.log(d)  // 12
